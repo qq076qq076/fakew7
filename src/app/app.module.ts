a02fefce130d2b0766716d24818dfb6c45114266
qq076qq076 <qq076qq076@gmail.com>
2019-04-12 19:10:07 +0800

    增加程式集元件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './component/toolbar/toolbar.component';
+import { ProgramsComponent } from './component/programs/programs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    ProgramsComponent
   ],
   imports: [
     BrowserModule
