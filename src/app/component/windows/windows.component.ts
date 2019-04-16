29892838f8a8a3c78073d73ad54e137f82dec907
david <qq076qq076@gmail.com>
2019-04-17 01:25:47 +0800

    變更視窗大小 施作中

--- src/app/component/windows/windows.component.ts
+++ src/app/component/windows/windows.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-windows',
   templateUrl: './windows.component.html',
   styleUrls: ['./windows.component.scss']
 })
-export class WindowsComponent implements OnInit {
-  constructor() { }
+export class WindowsComponent implements AfterViewInit {
+  constructor(
+    private elementRef: ElementRef
+  ) { }
   name = 'Windows Internet Explorer';
   logo = '/assets/images/ie-icon.png';
 
-  ngOnInit() {
+  @ViewChild('border-container') borderContainer: ElementRef;
+
+  ngAfterViewInit() {
+    // this.resizeRef = this.elementRef.nativeElement.querySelector('.border-container');
+    // console.log(this.resizeRef);
+  }
+
+  changeCursor(e) {
+    // let width = this.borderContainer.offsetWidth;
+    console.log(width)
   }
 
 }
