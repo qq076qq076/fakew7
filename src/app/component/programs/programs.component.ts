9b2d6aff19b1a99b4f372e6981edfaffdcebd369
qq076qq076 <qq076qq076@gmail.com>
2019-04-22 16:59:58 +0800

    程式集增加項目

--- src/app/component/programs/programs.component.ts
+++ src/app/component/programs/programs.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input, HostBinding } from '@angular/core';
+import { Component, OnInit, Input, HostBinding, HostListener, EventEmitter, Output } from '@angular/core';
+import { ApplicationService, IApplication } from 'src/app/service/application/application.service';
 
 @Component({
   selector: 'app-programs',
@@ -7,15 +8,45 @@ import { Component, OnInit, Input, HostBinding } from '@angular/core';
 })
 export class ProgramsComponent implements OnInit {
   @HostBinding('class.open') open: boolean;
+  @Output() toggleProgrames = new EventEmitter();
   @Input()
   set isOpen(open: boolean) {
     this.open = open;
   }
 
+  appList: IApplication[] = [];
 
-  constructor() { }
+  rightList = [
+    {
+      name: 'Guest',
+    },
+    {
+      name: '文件',
+    },
+    {
+      name: '圖片',
+    },
+    {
+      name: '音樂',
+    },
+    {
+      name: '電腦',
+    },
+  ]
+
+  constructor(
+    private appService: ApplicationService
+  ) {
+    appService.getApplication().subscribe((appList: IApplication[]) => {
+      this.appList = appList;
+    });
+  }
 
   ngOnInit() {
+  }
+
+  close() {
+    this.toggleProgrames.emit();
   }
 
 }
