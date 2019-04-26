e4fcfad69c37e4afae026f975f25e140e565be57
david <qq076qq076@gmail.com>
2019-04-27 01:51:58 +0800

    資料夾程式    路徑計算  顯示

--- src/app/application/folder/folder.component.ts
+++ src/app/application/folder/folder.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Type } from '@angular/core';
+import { Component, OnInit, Type, HostListener } from '@angular/core';
 
 interface Folder {
   path: string;
@@ -15,33 +15,62 @@ interface Folder {
 export class FolderComponent implements OnInit {
 
   constructor() { }
-  urlText = 'C:\\';
   focusePath = false;
-  fullPath = '\\';
-  folderMap: Folder = {
-    path: '\\',
-    icon: '',
-    directory: [
-      {
-        path: 'C:',
-        icon: '',
-        directory: [
-          {
-            path: 'Program Files',
-            icon: '',
-            directory: []
-          },
-          {
-            path: '使用者',
-            icon: '',
-            directory: []
-          },
-        ]
-      },
-    ],
-  };
+  folderMap: Folder[] = [
+    {
+      path: '電腦',
+      icon: '',
+      directory: [
+        {
+          path: 'C:',
+          icon: '',
+          directory: [
+            {
+              path: 'Program Files',
+              icon: '',
+              directory: []
+            },
+            {
+              path: '使用者',
+              icon: '',
+              directory: []
+            },
+          ]
+        },
+      ],
+    }
+  ];
+  // fullPathList: string[] = [this.folderMap[0].path];
+  fullPathList: string[] = ['電腦', 'C:', 'Program Files'];
+  urlText = '';
 
   ngOnInit() {
+    this.getUrlText();
   }
 
+  getUrlText(folderMap: Folder[] = this.folderMap, index: number = 0) {
+    const thisPath = folderMap.find((folder: Folder) => folder.path === this.fullPathList[index]);
+    if (this.urlText === '電腦') {
+      this.urlText = thisPath.path;
+    } else {
+      this.urlText += (this.urlText) ? '\\' + thisPath.path : thisPath.path;
+    }
+    if (this.fullPathList.length > index + 1) {
+      this.getUrlText(thisPath.directory, index + 1);
+    }
+  }
+
+  setFocuse() {
+    this.focusePath = true;
+  }
+
+  stopPropagation(e: MouseEvent) {
+    e.stopPropagation();
+  }
+
+  @HostListener('document:click') setUnFocus() {
+    if (this.focusePath) {
+      this.focusePath = false;
+    }
+  }
 }
