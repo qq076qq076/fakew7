cfcfb56cddce5d9deed55ecf0cb641ae926beecc
qq076qq076 <qq076qq076@gmail.com>
2019-05-25 22:49:16 +0800

    Auto-generated commit

--- out-tsc/src/app/type/drag-direction/drag-direction.spec.js
+++
@@ -1,7 +0,0 @@
-import { DragDirection } from './drag-direction';
-describe('DragDirection', function () {
-    it('should create an instance', function () {
-        expect(new DragDirection()).toBeTruthy();
-    });
-});
-//# sourceMappingURL=drag-direction.spec.js.map
